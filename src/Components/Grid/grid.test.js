e1f52341da84b11bac0f153d1d7b1ce0635a9cec
Alfie Darko <me@alfiedarko.co.uk>
2021-02-03 23:45:03 +0000

    Grid should only show correct grid items

--- src/Components/Grid/grid.test.js
+++ src/Components/Grid/grid.test.js
@@ -4,6 +4,24 @@ import Grid from './index'
 
 test('should show a grid', () => {
   render(<Grid />);
-  const linkElement = screen.getByTestId("grid");
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByTestId("grid");
+  expect(element).toBeInTheDocument();
+})
+
+test('should show correct grid items in the grid', () => {
+    render(<Grid itemAmount={3}/>);
+
+    const gridItem1 = screen.getByTestId("grid-item-1");
+    const gridItem2 = screen.getByTestId("grid-item-3");
+
+    expect(gridItem1).toBeInTheDocument();
+    expect(gridItem2).toBeInTheDocument();
+})
+
+test('should NOT show more grid items than the item amount', () => {
+    render(<Grid itemAmount={3}/>);
+
+    const gridItem = screen.queryByTestId("grid-item-4");
+    expect(gridItem).not.toBeInTheDocument();
+
 })
