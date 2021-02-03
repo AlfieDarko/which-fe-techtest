9ae6a16236e036a9c2a12f4a5b8724769a46cf6a
Alfie Darko <me@alfiedarko.co.uk>
2021-02-03 20:16:02 +0000

    Modified boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <h1>Multiplication Game!</h1>
+          <h2>Instructions: Click a number to show you all the multiples of that number</h2>
+        </header>
     </div>
   );
 }
