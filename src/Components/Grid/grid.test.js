559d8f31cefd7db59d659049816c12615e2ac35d
Alfie Darko <me@alfiedarko.co.uk>
2021-02-04 20:36:34 +0000

    Allowed Grid to show Grid Items that are common Multiples of the clicked button onClick

--- src/Components/Grid/grid.test.js
+++ src/Components/Grid/grid.test.js
@@ -1,27 +1,45 @@
-import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 
+import {
+  fireEvent,
+  getByTestId,
+  render,
+  screen
+} from '@testing-library/react'
+
+import React from 'react'
 import Grid from './index'
 
 test('should show a grid', () => {
-  render(<Grid />);
-  const element = screen.getByTestId("grid");
-  expect(element).toBeInTheDocument();
+  render(<Grid />)
+  const element = screen.getByTestId('grid')
+  expect(element).toBeInTheDocument()
 })
 
 test('should show correct grid items in the grid', () => {
-    render(<Grid itemAmount={3}/>);
+  render(<Grid numberOfItems={3} />)
 
-    const gridItem1 = screen.getByTestId("grid-item-1");
-    const gridItem2 = screen.getByTestId("grid-item-3");
+  const gridItem1 = screen.getByTestId('grid-item-1')
+  const gridItem2 = screen.getByTestId('grid-item-3')
 
-    expect(gridItem1).toBeInTheDocument();
-    expect(gridItem2).toBeInTheDocument();
+  expect(gridItem1).toBeInTheDocument()
+  expect(gridItem2).toBeInTheDocument()
 })
 
 test('should NOT show more grid items than the item amount', () => {
-    render(<Grid itemAmount={3}/>);
+  render(<Grid numberOfItems={3} />)
 
-    const gridItem = screen.queryByTestId("grid-item-4");
-    expect(gridItem).not.toBeInTheDocument();
+  const gridItem = screen.queryByTestId('grid-item-4')
+  expect(gridItem).not.toBeInTheDocument()
+})
 
+test('should highlight grid items that are a common multiple', async () => {
+  render(<Grid numberOfItems={144} />)
+
+  const gridItem144 = screen.getByTestId('grid-item-144')
+
+  fireEvent.click(gridItem144)
+
+  const gridItem = await screen.queryByTestId('grid-item-144')
+  expect(gridItem).toHaveClass('grid-item--state-highlighted')
 })
--- src/Components/Grid/index.js
+++ src/Components/Grid/index.js
@@ -1,19 +1,45 @@
 import './grid.scss'
 
+import React, { useState } from 'react'
+
 import GridItem from '../GridItem'
-import React from 'react'
+import findMultiples from '../../utils/findMultiples'
 
-const Grid = ({ numberOfItems}) => {
+const Grid = ({ numberOfItems }) => {
+  const [clickedItemValue, setClickedItemValue] = useState()
 
-    const renderGridItems = (numberOfItems) => {
-      return [...Array(numberOfItems)].map((element, index) => (<GridItem number={index+1} />))
+  const renderGridItems = (numberOfItems) => {
+    const gridItems = [...Array(numberOfItems).keys()].map(i => i + 1)
+
+    if (clickedItemValue) {
+      const multiplesList = findMultiples(numberOfItems, clickedItemValue)
+
+      return gridItems.map((element) => {
+        const isCommonMultiple = multiplesList.includes(element)
+        return <GridItem
+          key={element}
+          number={element}
+          setClickedItemValue={setClickedItemValue}
+          isCommonMultiple={isCommonMultiple}
+        />
+      })
     }
 
-    return (
+    return gridItems.map((element) => {
+      return <GridItem
+          key={element}
+          number={element}
+          setClickedItemValue={setClickedItemValue}
+          isCommonMultiple={false}
+        />
+    })
+  }
+
+  return (
         <div className="grid" data-testid="grid">
           {renderGridItems(numberOfItems)}
         </div>
-    )
+  )
 }
 
 export default Grid
