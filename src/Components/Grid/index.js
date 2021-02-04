cdfdd2f2a19c134b96b008de8f750c582527b435
Alfie Darko <me@alfiedarko.co.uk>
2021-02-04 00:43:19 +0000

    Refactored the Grid component

--- src/Components/Grid/index.js
+++ src/Components/Grid/index.js
@@ -4,9 +4,14 @@ import GridItem from '../GridItem'
 import React from 'react'
 
 const Grid = ({ numberOfItems}) => {
+
+    const renderGridItems = (numberOfItems) => {
+      return [...Array(numberOfItems)].map((element, index) => (<GridItem number={index+1} />))
+    }
+
     return (
         <div className="grid" data-testid="grid">
-            
+          {renderGridItems(numberOfItems)}
         </div>
     )
 }
