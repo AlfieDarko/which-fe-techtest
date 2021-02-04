cddbc7996aed9bbf736c9ddd6e8c2e52b882d933
Alfie Darko <me@alfiedarko.co.uk>
2021-02-04 20:06:04 +0000

    Added Unhappy Path tests for findMultiples()

--- src/utils/findMultiples.js
+++ src/utils/findMultiples.js
@@ -1,6 +1,14 @@
 const findMultiples = (maxNumber, multipleOfNumber) => {
-    const numbers = [ ...Array(maxNumber).keys()].map(i => i+1)
-    return numbers.filter(element => !(element % multipleOfNumber));
+  if (maxNumber === undefined || typeof maxNumber !== 'number') {
+    throw new Error(`you must provide a number for input maxNumber. Received type: ${typeof maxNumber}`)
+  } else
+
+  if (multipleOfNumber === undefined || typeof multipleOfNumber !== 'number') {
+    throw new Error(`you must provide a number for input multipleOfNumber. Received type: ${typeof multipleOfNumber}`)
+  }
+
+  const numbers = [...Array(maxNumber).keys()].map(i => i + 1)
+  return numbers.filter(element => !(element % multipleOfNumber))
 }
 
 export default findMultiples
--- src/utils/findMultiples.test.js
+++ src/utils/findMultiples.test.js
@@ -1,17 +1,24 @@
 import findMultiples from './findMultiples'
 
 test('should find the correct common multiples of 144 (itself)', () => {
-    let result = findMultiples(144, 144)
-    expect(result).toEqual([144])
+  const result = findMultiples(144, 144)
+  expect(result).toEqual([144])
 })
 
-test('should should find the correct common multiples of 3', () => {
-    let result = findMultiples(144, 6)
-    expect(result).toEqual([6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126,132,138,144])
+test('should  find the correct common multiples of 3', () => {
+  const result = findMultiples(144, 6)
+  expect(result).toEqual([6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 126, 132, 138, 144])
 })
 
-test('should should find the correct common multiples of 77', () => {
-    let result = findMultiples(144, 77)
-    expect(result).toEqual([77])
+test('should find the correct common multiples of 77', () => {
+  const result = findMultiples(144, 77)
+  expect(result).toEqual([77])
 })
 
+test('should throw error if first input (maxNumber) isnt a number', () => {
+  expect(() => findMultiples('144', 77)).toThrow('you must provide a number for input maxNumber. Received type: string')
+})
+
+test('should throw error if last input (multipleOfNumber) isnt a number', () => {
+  expect(() => findMultiples(144, '77')).toThrow('you must provide a number for input multipleOfNumber. Received type: string')
+})
