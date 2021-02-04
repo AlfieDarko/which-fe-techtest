459da8d78678b4934f961058d88f404b51abddaa
Alfie Darko <me@alfiedarko.co.uk>
2021-02-04 22:36:00 +0000

    Versioning untracked files

--- src/components/Layout/layout.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-
-import Layout from '.'
-
-test('should render correctly', () => {
-  const { asFragment } = render(<Layout />)
-  expect(asFragment()).toMatchSnapshot()
-})
