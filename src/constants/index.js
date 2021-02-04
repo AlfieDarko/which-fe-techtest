ae9447f7db03f93845f42ffb26ad27cbdcfc5c55
Alfie Darko <me@alfiedarko.co.uk>
2021-02-04 21:58:46 +0000

    Added error strings to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,4 @@
 export const HEADER_TITLE = 'Multiplication Game!'
 export const HEADER_SUBTITLE = 'Instructions: Click a number to show you all the multiples of that number'
+export const MAX_NUMBER_TYPE_ERROR = 'you must provide a number for input maxNumber. Received type: '
+export const MULTIPLE_OF_NUMBER_TYPE_ERROR = 'you must provide a number for input multipleOfNumber. Received type: '
--- src/utils/findMultiples.js
+++ src/utils/findMultiples.js
@@ -1,10 +1,12 @@
+import { MAX_NUMBER_TYPE_ERROR, MULTIPLE_OF_NUMBER_TYPE_ERROR } from '../constants'
+
 const findMultiples = (maxNumber, multipleOfNumber) => {
   if (maxNumber === undefined || typeof maxNumber !== 'number') {
-    throw new Error(`you must provide a number for input maxNumber. Received type: ${typeof maxNumber}`)
+    throw new Error(MAX_NUMBER_TYPE_ERROR + `${typeof maxNumber}`)
   } else
 
   if (multipleOfNumber === undefined || typeof multipleOfNumber !== 'number') {
-    throw new Error(`you must provide a number for input multipleOfNumber. Received type: ${typeof multipleOfNumber}`)
+    throw new Error(MULTIPLE_OF_NUMBER_TYPE_ERROR + `${typeof multipleOfNumber}`)
   }
 
   const numbers = [...Array(maxNumber).keys()].map(i => i + 1)
