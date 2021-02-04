2cf41af08bd4377e171c68f5b12cabd0ea1c42f4
Alfie Darko <me@alfiedarko.co.uk>
2021-02-04 20:33:18 +0000

    Update GridItem to change CSS upon the isCommonMultiple prop

--- src/Components/GridItem/gridItem.test.js
+++ src/Components/GridItem/gridItem.test.js
@@ -1,16 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react'
+import React from 'react'
 
-import GridItem from './';
+import GridItem from '.'
 
-test('renders a grid item', () => {
-  render(<GridItem number="1"/>);
-  const linkElement = screen.getByTestId("grid-item-1");
-  expect(linkElement).toBeInTheDocument();
-});
+test('should render correctly', () => {
+  const { asFragment } = render(<GridItem number="1"/>)
+  expect(asFragment()).toMatchSnapshot()
+})
 
+test('should render a grid item', () => {
+  render(<GridItem number="1"/>)
+  const linkElement = screen.getByTestId('grid-item-1')
+  expect(linkElement).toBeInTheDocument()
+})
 
-test('renders a grid item with a value', () => {
-  render(<GridItem number="2"/>);
-  const linkElement = screen.getByTestId("grid-item-value");
-  expect(linkElement).toBeInTheDocument();
-});
+test('should render a grid item with a value', () => {
+  render(<GridItem number="2"/>)
+  const linkElement = screen.getByTestId('grid-item-value')
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('should render the grid-item correctly given the correct props', () => {
+  const { asFragment } = render(<GridItem number="1" isCommonMultiple/>)
+  expect(asFragment()).toMatchSnapshot()
+})
--- src/Components/GridItem/index.jsx
+++ src/Components/GridItem/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import './gridItem.scss'
 
-const GridItem = (props) => {
-    return (
-        <div data-testid={`grid-item-${props.number}`}>
-            <p data-testid={`grid-item-value`}>{props.number}</p>
-        </div>
-    )
+import React from 'react'
+import cx from 'classnames'
+
+const GridItem = ({ number, setClickedItemValue, isCommonMultiple }) => {
+  const onClickHandler = () => {
+    setClickedItemValue(number)
+  }
+
+  const gridItemClassName = cx({ 'grid-item': !isCommonMultiple }, { 'grid-item--state-highlighted': isCommonMultiple })
+
+  return (
+    <button type="button" className={gridItemClassName} value={number} data-testid={`grid-item-${number}`} onClick={() => onClickHandler()}>
+      <p data-testid="grid-item-value">{number}</p>
+    </button>
+  )
 }
 
 export default GridItem
