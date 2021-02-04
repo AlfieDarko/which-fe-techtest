d27df83c1df162647726c320a4e113d8382114c6
Alfie Darko <me@alfiedarko.co.uk>
2021-02-04 21:28:00 +0000

    Added Missing Proptypes to the gridItem component and beefed up the font size

--- src/Components/GridItem/gridItem.test.js
+++ src/Components/GridItem/gridItem.test.js
@@ -4,23 +4,23 @@ import React from 'react'
 import GridItem from '.'
 
 test('should render correctly', () => {
-  const { asFragment } = render(<GridItem number="1"/>)
+  const { asFragment } = render(<GridItem number={1}/>)
   expect(asFragment()).toMatchSnapshot()
 })
 
 test('should render a grid item', () => {
-  render(<GridItem number="1"/>)
+  render(<GridItem number={1}/>)
   const linkElement = screen.getByTestId('grid-item-1')
   expect(linkElement).toBeInTheDocument()
 })
 
 test('should render a grid item with a value', () => {
-  render(<GridItem number="2"/>)
+  render(<GridItem number={2}/>)
   const linkElement = screen.getByTestId('grid-item-value')
   expect(linkElement).toBeInTheDocument()
 })
 
 test('should render the grid-item correctly given the correct props', () => {
-  const { asFragment } = render(<GridItem number="1" isCommonMultiple/>)
+  const { asFragment } = render(<GridItem number={1} isCommonMultiple/>)
   expect(asFragment()).toMatchSnapshot()
 })
--- src/Components/GridItem/index.jsx
+++ src/Components/GridItem/index.jsx
@@ -1,20 +1,26 @@
-import './gridItem.scss'
-
 import React from 'react'
+import PropTypes from 'prop-types'
 import cx from 'classnames'
+import './gridItem.scss'
 
 const GridItem = ({ number, setClickedItemValue, isCommonMultiple }) => {
   const onClickHandler = () => {
     setClickedItemValue(number)
   }
 
-  const gridItemClassName = cx({ 'grid-item': !isCommonMultiple }, { 'grid-item--state-highlighted': isCommonMultiple })
+  const gridItemClassName = cx({ 'grid-item': !isCommonMultiple }, { 'grid-item grid-item--state-highlighted': isCommonMultiple })
 
   return (
     <button type="button" className={gridItemClassName} value={number} data-testid={`grid-item-${number}`} onClick={() => onClickHandler()}>
-      <p data-testid="grid-item-value">{number}</p>
+      <p className={'grid-item__value'} data-testid="grid-item-value">{number}</p>
     </button>
   )
+}
+
+GridItem.propTypes = {
+  number: PropTypes.number,
+  setClickedItemValue: PropTypes.func,
+  isCommonMultiple: PropTypes.bool
 }
 
 export default GridItem
